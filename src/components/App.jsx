866dc0bb294669567707c50c5370edee49bed2fd
czke33 <czke33@gmail.com>
2024-05-17 08:55:59 +0300

    update app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,53 +1,25 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import style from "./app.module.css";
 import ContactForm from "./ContactForm/ContactForm";
 import Filter from "./Filter/Filter";
 import ContactList from "./ContactList/ContactList";
+
+import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
  
 const KEY = "Contacts";
 
 const App = () => {
-	const [contacts, setContacts] = useState([]);
-	const [filter, setFilter] = useState("");
-	const isMounted = useRef(false);
+	const contacts = useSelector(getContacts);
+  const isMounted = useRef(false);
 
-	useEffect(() => {
-		const savedContacts = JSON.parse(localStorage.getItem(KEY));
-		savedContacts && setContacts([...savedContacts]);
-	}, []);
-
-	useEffect(() => {
-		if (isMounted.current) {
-			localStorage.setItem(KEY, JSON.stringify(contacts));
-		} else {
-			isMounted.current = true;
-		}
-	}, [contacts]);
-
-	const checkContact = (newContact) => {
-		const isInBase = contacts.some((contact) => contact.name === newContact.name);
-		return isInBase;
-	};
-
-	const addContact = (newContact) => {
-		const check = checkContact(newContact);
-		if (!check) {
-			let actualContacts = contacts;
-			actualContacts.push(newContact);
-			setContacts([...actualContacts]);
-		} else {
-			alert(`${newContact.name} is alerdy in contacts`);
-		}
-	};
-
-	const changeFilterValue = (e) => {
-		setFilter(e.target.value);
-	};
-
-	const deleteUser = (e) => {
-		const filteredContacts = contacts.filter((contact) => contact.id !== e.target.id);
-		setContacts(filteredContacts);
-	};
+  useEffect(() => {
+    if (isMounted.current) {
+      localStorage.setItem(KEY, JSON.stringify(contacts));
+    } else {
+      isMounted.current = true;
+    }
+  }, [contacts]);
 
     return (
       <div className={style.container}>
